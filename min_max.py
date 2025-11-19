8c9622ea092b0f62053089904e2f811076d019cd
Joe <nathanielbandi636@gmail.com>
2025-11-19 09:58:15 +0530

    Update min_max.py

--- min_max.py
+++ min_max.py
@@ -1,19 +1,19 @@
 import sys
-if len(sys.argv==3):
-    file_name=sys.argv[0]
-    a=int(sys.argv[1])
-    b=int(sys.argv[2])
+
+if len(sys.argv) == 3:
+    file_name = sys.argv[0]
+    a = int(sys.argv[1])
+    b = int(sys.argv[2])
 else:
-    a=10
-    b=20
+    a = 10
+    b = 20
     print("Default values taken")
 
-if(a>b):
-        print(f"Max value is {a}")
-else if(a<b):
-        print(f"Max value is {b}")
-else if(a==b):
-        print("Both values are equal")
+if a > b:
+    print(f"Max value is {a}")
+elif a < b:
+    print(f"Max value is {b}")
+elif a == b:
+    print("Both values are equal")
 else:
-     print("Invalid Input")
-     
+    print("Invalid Input")
